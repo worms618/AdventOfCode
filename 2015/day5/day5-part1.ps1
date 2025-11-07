7b6b004000e6f368669de89c44c6b725256377f5
Remco Vorthoren <remco.boys@gmail.com>
2025-11-07 13:28:34 +0100

    2015 - Day 5

---
+++ 2015/day5/day5-part1.ps1
@@ -0,0 +1,82 @@
+﻿# https://adventofcode.com/2015/day/5
+
+$InputFilePath = "..\..\inputs\2015\day5\input.txt"
+
+$NiceStrings = 0
+
+
+$ForbiddenParts = @("ab", "cd", "pq", "xy")
+$Vowels = @("a", "e", "i", "o", "u")
+
+function ContainsForbiddenPart{
+    param([string]$value)
+
+    foreach($Part in $ForbiddenParts) {
+        if ($Value -match $Part) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function CountVowels{
+    param([string]$value)
+
+    $total = 0
+
+    for($i = 0; $i -lt $value.length; $i++) {
+        $character = $value[$i]
+
+        if ($Vowels -contains $character) {
+            $total++
+        }
+    }
+
+    return $total    
+}
+
+function ContainsLetterTwiceInRow{
+    param([string]$value)
+    
+    for($i = 0; $i -lt $value.length - 1; $i++) {
+        $currentChar = $value[$i]
+        $nextChar = $value[$i + 1]
+
+        #Write-Host $currentChar, $nextChar
+
+        if ($currentChar -eq $nextChar) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+$InputContent = Get-Content -Path:$InputFilePath | ForEach-Object {
+    $Input = $_
+
+    #Write-Host $Input
+
+    $HasForbiddenPart = ContainsForbiddenPart -value: $Input
+    #Write-Host "HasForbiddenPart:", $HasForbiddenPart
+
+    if ($HasForbiddenPart -eq $false) {
+        $TotalVowels = CountVowels -value: $Input
+        #Write-Host "TotalVowels:", $TotalVowels
+
+        $HasLetterTwiceInRow = ContainsLetterTwiceInRow -value: $Input
+        #Write-Host "HasLetterTwiceInRow:", $HasLetterTwiceInRow
+
+        $HasEnoughTotalVowels = $TotalVowels -ge 3
+        #Write-Host "HasEnoughTotalVowels:", $HasEnoughTotalVowels
+
+        if ($HasEnoughTotalVowels -and $HasLetterTwiceInRow) {
+            $NiceStrings = $NiceStrings + 1
+        }
+    } else {
+        $Forbid = $Forbid + 1;
+    }
+}
+
+Write-Host "Nice strings:", $NiceStrings
---
+++ 2015/day5/day5-part2.ps1
@@ -0,0 +1,81 @@
+﻿# https://adventofcode.com/2015/day/5
+
+$InputFilePath = "..\..\inputs\2015\day5\input.txt"
+
+$NiceStrings = 0
+
+function ContainsOneLetterRepeatingWithOneLetterInBetween {
+    param([string]$value)
+
+    for($i = 0; $i -lt $value.length - 2; $i++) {
+        $currentChar = $value[$i]
+        $nextNextChar = $value[$i + 2]
+
+        #Write-Host $currentChar, $nextNextChar
+
+        $charIsRepeated = $currentChar -eq $nextNextChar
+
+        if ($charIsRepeated) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function ContainsPairOfLettersRepeatedWithoutOverlap {
+    param([string]$value)
+
+    # Write-Host $value
+
+    for($i = 0; $i -lt $value.length - 2; $i++) {
+        $currentChar = $value[$i]
+        $nextChar = $value[$i + 1]
+        $nextPairIndex = $i + 2
+
+        $pair = $currentChar + $nextChar
+        #Write-Host $pair
+
+        $PairOfLettersRepeated = ContainsPairOfLetters -value: $value -pairToFind: $pair -fromIndex: $nextPairIndex
+        
+        #Write-Host "PairOfLettersRepeated:", $PairOfLettersRepeated
+
+        if ($PairOfLettersRepeated -eq $true) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function ContainsPairOfLetters {
+    param([string]$value, [string]$pairToFind, [int]$fromIndex)
+    
+    for($i = $fromIndex; $i -lt $value.length - 1; $i++) {
+        $currentChar = $value[$i]
+        $nextChar = $value[$i + 1]
+        $pair = $currentChar + $nextChar
+
+        #Write-Host $pairToFind, $pair
+
+        if ($pairToFind -eq $pair) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+$InputContent = Get-Content -Path:$InputFilePath | ForEach-Object {
+    $HasOneLetterRepeatingWithOneLetterInBetween = ContainsOneLetterRepeatingWithOneLetterInBetween -value: $_
+    $HasPairOfLettersRepeatedWithoutOverlap = ContainsPairOfLettersRepeatedWithoutOverlap -value: $_
+
+    #Write-Host $_
+    #Write-Host "HasOneLetterRepeatingWithOneLetterInBetween:", $HasOneLetterRepeatingWithOneLetterInBetween, "|HasPairOfLettersRepeatedWithoutOverlap:", $HasPairOfLettersRepeatedWithoutOverlap
+
+    if ($HasOneLetterRepeatingWithOneLetterInBetween -and $HasPairOfLettersRepeatedWithoutOverlap) {
+        $NiceStrings = $NiceStrings + 1
+    }
+}
+
+Write-Host "Nice strings:", $NiceStrings
